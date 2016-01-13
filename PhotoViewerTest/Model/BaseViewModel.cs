8c8b2d6644ff548a9cab39072fb996fdedf38853
Jonas Frid <jonas.frid@softgear.se>
2016-01-13 11:54:53 +0100

    Refactoring

--- PhotoViewerTest/Model/BaseViewModel.cs
+++ PhotoViewerTest/Model/BaseViewModel.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.ComponentModel;
-using Xamarin.Forms;
-using System.Threading.Tasks;
-using System.Runtime.CompilerServices;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace PhotoViewerTest
 {
@@ -16,20 +16,31 @@ namespace PhotoViewerTest
             return Task.FromResult (0);
         }
 
-        protected void OnPropertyChanged(string propertyName) {
-            if (PropertyChanged == null) return;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged == null)
+            {
+                return;
+            }
+
             PropertyChanged (this, new PropertyChangedEventArgs (propertyName));
         }
 
         protected void SetObservableProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
         {
-            if (EqualityComparer<T>.Default.Equals(field, value)) return;
+            if(EqualityComparer<T>.Default.Equals(field, value))
+            {
+                return;
+            }
+
             field = value;
             OnPropertyChanged (propertyName);
         }
 
         #region INotifyPropertyChanged implementation
+
         public event PropertyChangedEventHandler PropertyChanged;
+
         #endregion
     }
 }
